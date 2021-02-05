7c8cfc9d52424c9dca34a78daae085c0ac52e6ac
Xiloer <83123095@qq.com>
2021-02-05 16:44:53 +0800

    WebServer创建项目

---
+++ V1/src/main/java/com/webserver/core/WebServer.java
@@ -0,0 +1,15 @@
+package com.webserver.core;
+
+/**
+ * 小鸟WebServer
+ * 实现Tomcat的基础功能的一个Web容器
+ * Web容器的作用:
+ * 1:Web容器是一个Web服务端程序,负责与客户端(通常是浏览器)进行交互
+ * 2:完成与客户端的TCP连接及数据交互.
+ * 3:基于HTTP协议与客户端进行应用交互,使得浏览器可以访问Web容器中部署的不同网络应用(Webapp)
+ *   的页面,资源,功能等.
+ * 4:可以管理部署多个不同的网络应用.
+ */
+public class WebServer {
+
+}
