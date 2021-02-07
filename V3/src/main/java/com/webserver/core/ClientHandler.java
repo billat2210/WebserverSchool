3d5ca512eade8afa2846c7bc25054349489c55e9
Xiloer <83123095@qq.com>
2021-02-07 11:53:12 +0800

    WebServer_v3

--- V3/src/main/java/com/webserver/core/ClientHandler.java
+++ V3/src/main/java/com/webserver/core/ClientHandler.java
@@ -22,12 +22,20 @@ public class ClientHandler implements Runnable{
             InputStream in = socket.getInputStream();
             //测试读取客户端发送过来的请求内容
             int d;
+            char cur=' ';//表示本次读取到的字符
+            char pre=' ';//表示上次读取到的字符
+            StringBuilder builder = new StringBuilder();//保存读取到的所有字符
             while ((d = in.read())!=-1){
-                char c = (char)d;
-                System.out.print(c);
+                cur = (char)d;//本次读取到的字符
+                //如果上次读取的是回车符，本次读取的是换行符则停止读取
+                if(pre==13 && cur==10){
+                    break;
+                }
+                builder.append(cur);
+                pre = cur;
             }
-
-
+            String line = builder.toString().trim();
+            System.out.println(line);
             //2处理请求
 
             //3发送响应
